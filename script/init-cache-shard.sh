614477434be47786c9783eab1e7e1c5bf85f1d52
Gabor Retvari <retvari@tmit.bme.hu>
2025-01-07 17:57:20 +0100

    test: Use configurable memcached memory, enforce a single thread

--- script/init-cache-shard.sh
+++ script/init-cache-shard.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-TABLE_DUMP=test_table.dump
-export PGPASSWORD=test
-
-USAGE="init-cache-shard.sh <num-memcached-servers>"
-[ -z "$1" ] && echo $USAGE && exit 0
+USAGE="init-cache-shard.sh <num-memcached-servers> <memory-MB>"
+[ -z "$1" -o -z "$2" ] && echo $USAGE && exit 0
 MEMCACHED_SERVER_NUM="$1"
+MEMORY="$2"
 
 cd "$(dirname "$0")"
 
 echo "Start memcached"
 killall memcached
 sleep 1
-for p in $(seq 11211 $((11211+MEMCACHED_SERVER_NUM))); do
-    memcached -l localhost -p $p -U $p &
+for p in $(seq 11211 $((11211+MEMCACHED_SERVER_NUM-1))); do
+    memcached -l localhost -m $MEMORY -t 1 -p $p -U $p &
 done
 
 exit 1
